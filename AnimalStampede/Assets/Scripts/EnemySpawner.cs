9deb6948fe64762eed487750186b6403d6605c04
Switchkid64 <minaj@roverkids.org>
2025-05-15 13:40:25 -0400

    back to where I was before I lost my progress
    mostly

--- AnimalStampede/Assets/Scripts/EnemySpawner.cs
+++ AnimalStampede/Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public int animalIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +15,9 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
+        if (Input.GetKeyDown(KeyCode.Delete)) {
+            //randomly generate spawn location and object
+            Vector3.spawmnpos = new Vector3(Random.Range(-spawnRangex, spawnRangeX, SpawnPosZ));
             int animalIndex = Random.Range(0, 3);
 
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 40), animalPrefabs[animalIndex].transform.rotation);
