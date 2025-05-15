711da5e87072b6fc10144fd599bbc9955518f74f
Switchkid64 <minaj@roverkids.org>
2025-05-15 13:02:47 -0400

    2.3 started

---
+++ AnimalStampede/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            int animalIndex = Random.Range(0, 3);
+
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 40), animalPrefabs[animalIndex].transform.rotation);
+        }
+        
+    }
+}
