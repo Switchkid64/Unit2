c263d695917f3d23aab95b4595c4b9b187a6ae20
Switchkid64 <minaj@roverkids.org>
2025-05-16 14:19:34 -0400

    cleaned up code a bit

--- AnimalStampede/Assets/Scripts/EnemySpawner.cs
+++ AnimalStampede/Assets/Scripts/EnemySpawner.cs
@@ -17,7 +17,7 @@ public class EnemySpawner : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Delete)) {
             //randomly generate spawn location and object
-            Vector3.spawmnpos = new Vector3(Random.Range(-spawnRangex, spawnRangeX, SpawnPosZ));
+            Vector3.spawmnpos = new Vector3(Random.Range(-spawnRangex, spawnRangeX), SpawnPosZ, 2);
             int animalIndex = Random.Range(0, 3);
 
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 40), animalPrefabs[animalIndex].transform.rotation);
