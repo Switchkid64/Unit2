e524c05874a4395d90246324fc61969d5920b77b
Switchkid64 <minaj@roverkids.org>
2025-05-27 14:11:24 -0400

    started the last section

--- AnimalStampede/Assets/Scripts/EnemySpawner.cs
+++ AnimalStampede/Assets/Scripts/EnemySpawner.cs
@@ -7,24 +7,28 @@ public class EnemySpawner : MonoBehaviour
     public GameObject[] animalPrefabs;
     public int animalIndex;
     public Vector3 spawnpos;
-    public float spawnRangeX;
-    public float SpawnPosZ;
+    private float spawnRangeX;
+    private float SpawnPosZ;
+    public float betweenSpawnTime;
+    public float spawnStartDelayTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", spawnStartDelayTime, betweenSpawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Delete)) {
-            //randomly generate spawn location and object
-            Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), SpawnPosZ, 7);
-            int animalIndex = Random.Range(0, 7);
-
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 40), animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
-        
+    }
+    //random animals spawn on their own
+    void SpawnRandomAnimal()  {
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), SpawnPosZ, 7);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 40), animalPrefabs[animalIndex].transform.rotation);
     }
 }
